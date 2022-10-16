e2c0f924255d043a18f514e151b87d991462cdd8
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2022-10-16 18:27:55 +0100

    add installation for carousel

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # thread-to-carousel
+
+## Installation
+
+1. Create new environment:
+```
+conda create --name thread
+```
+
+2. Activate new environment:
+```
+conda activate thread
+```
+
+3. Install python and poetry
+```
+conda install python poetry
+```
+
+4. Let poetry install all dependencies
+```
+poetry install
+```
---
+++ convert2carousel.py
@@ -0,0 +1,19 @@
+import requests
+import pandas as pd
+from datetime import datetime,timedelta
+import tweepy
+import io
+import os
+import requests
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+from matplotlib import pyplot as plt
+import numpy as np
+import textwrap
+import re
+import emoji
+from PyPDF2 import PdfMerger
+import os
+from dotenv import load_dotenv
+
+if __name__ == '__main__':
+    print("Hello World")
